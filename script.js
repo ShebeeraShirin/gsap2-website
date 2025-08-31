1d0c772b7c37aee70b4da2db7b2c324d883f929b
ShebeeraShirin <shebeerashirin@gmail.com>
2025-08-31 22:38:54 +0530

    gsap2

---
+++ script.js
@@ -0,0 +1,42 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from("#img3",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
+gsap.from("#main h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+
+})
+gsap.from("#img4",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:60
+})
+gsap.from("#img5",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    x:60
+})
+gsap.from("#img6",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+    y:-60
+})
